c542d21c14b8ddd5706b2f71e4561a40ea1593cc
Keyur1291 <pkeyur2138@gmail.com>
2024-11-12 02:02:23 +0530

    Added new features such as list of meals based on categories, meal details, new app bars, etc.

--- app/src/main/java/com/example/recipeapp/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.recipeapp
-
-sealed class Screen(val route: String) {
-
-    data object RecipeScreen: Screen("RecipeScreen")
-    data object DetailScreen: Screen("DetailScreen")
-
-}
